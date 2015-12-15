de1e4a2f36fee9c4752c7d3d485e9feb86af5773
Sergey Mamonov <samamonov@gmail.com>
2015-12-15 22:24:51 +0300

    cache helper added

--- app/src/main/java/com/merann/smamonov/googledrive/managers/DiskCacheHelper.java
+++ app/src/main/java/com/merann/smamonov/googledrive/managers/DiskCacheHelper.java
@@ -1,12 +1,12 @@
 package com.merann.smamonov.googledrive.managers;
 
 import android.content.Context;
+import android.graphics.Bitmap;
 import android.util.Log;
-
-import org.apache.commons.io.filefilter.DirectoryFileFilter;
 
 import java.io.File;
 import java.io.FileFilter;
+import java.io.FileOutputStream;
 
 /**
  * Created by samam_000 on 15.12.2015.
@@ -17,8 +17,15 @@ public class DiskCacheHelper {
     private final static String CacheDirectoryForIcons = "IconsCache";
     private final static String CacheDirectoryForImages = "ImagesCache";
 
-    private static File getDiskCacheDir(Context context, String subfolder) {
+    private File mIconCacheFolder;
+    private File mImageCacheFolder;
 
+    DiskCacheHelper(Context context) {
+        CacheDirectoryForIcons(context);
+        CacheDirectoryForImages(context);
+    }
+
+    private static File getDiskCacheDir(Context context, String subfolder) {
         final File cacheDirectory = context.getCacheDir();
         File result = null;
 
@@ -37,19 +44,15 @@ public class DiskCacheHelper {
                 }
             }
 
-            if(result == null)
-            {
+            if (result == null) {
                 result = new File(cacheDirectory.getPath() + "/" + cacheDirectory);
-                if (result.mkdir())
-                {
+                if (result.mkdir()) {
                     Log.d(LOG_TAG, "Cache subfolder "
                             + cacheDirectory
                             + " was created");
-                }
-                else
-                {
+                } else {
                     Log.e(LOG_TAG, "Unable to create subfolder "
-                    + cacheDirectory);
+                            + cacheDirectory);
                 }
             }
         } else {
@@ -59,14 +62,76 @@ public class DiskCacheHelper {
         return result;
     }
 
-    private static File CacheDirectoryForIcons(Context context) {
-        return getDiskCacheDir(context, CacheDirectoryForIcons);
+    private File CacheDirectoryForIcons(Context context) {
+        mIconCacheFolder = getDiskCacheDir(context, CacheDirectoryForIcons);
+        return mIconCacheFolder;
     }
 
-    private static File CacheDirectoryForImages(Context context) {
-        return getDiskCacheDir(context, CacheDirectoryForImages);
+    private File CacheDirectoryForImages(Context context) {
+        mImageCacheFolder = getDiskCacheDir(context, CacheDirectoryForImages);
+        return mImageCacheFolder;
+    }
+
+    public File getCachedIcon(String filename) {
+        return getFileFromFolder(mIconCacheFolder, filename);
+    }
+
+    public File getCachedImage(String filename) {
+        return getFileFromFolder(mImageCacheFolder, filename);
+    }
+
+    private static File getFileFromFolder(File folder, final String filename) {
+        File[] results = folder.listFiles(new FileFilter() {
+            @Override
+            public boolean accept(File pathname) {
+                return filename.equals(pathname.getName());
+            }
+        });
+
+        return results.length == 0 ? results[0] : null;
+    }
+
+    private static File createNewFile(File folder, String filename) {
+        File result = new File(folder, filename);
+        return result;
+    }
+
+    private File createNewIconFile(String filename) {
+        return createNewFile(mIconCacheFolder, filename);
+    }
+
+    private File createNewImageFile(String filename) {
+        return createNewFile(mImageCacheFolder, filename);
     }
 
 
+    private static void saveBitmapToFile(File file, Bitmap bitmap) {
+        try {
+            FileOutputStream fileOutputStream = new FileOutputStream(file);
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, fileOutputStream);
+            fileOutputStream.close();
+        } catch (Throwable throwable) {
+            Log.e(LOG_TAG, "Unable to save bitmap: "
+                    + throwable.getMessage());
+        }
+    }
 
+    public void saveIconToFile(String filename, Bitmap bitmap) {
+        File newFile = getCachedIcon(filename);
+
+        if (filename == null) {
+            newFile = createNewIconFile(filename);
+        }
+        saveBitmapToFile(newFile, bitmap);
+    }
+
+    public void saveImageToFile(String filename, Bitmap bitmap) {
+
+        File newFile = getCachedImage(filename);
+
+        if (filename == null) {
+            newFile = createNewImageFile(filename);
+        }
+        saveBitmapToFile(newFile, bitmap);
+    }
 }
